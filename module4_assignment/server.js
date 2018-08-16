7cf0e5b765b1cdfd6ab45a44b5fd08d78543e390
Patrik Lilja <lilja.patrik@gmail.com>
2018-08-16 20:41:14 +0200

    Upgrade packages in attempt to fix transitive package vulnerability

--- module4_assignment/server.js
+++ module4_assignment/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/edx-course-db', {useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/edx-course-db')
 
 const Account = mongoose.model('Account',
     {
